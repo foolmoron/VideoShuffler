fa27511ba90d41b0ef73405a9a95ff08c4ee368a
Momin Khan <foolmoron@gmail.com>
2023-12-06 00:57:43 -0500

    interrupts

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import https from 'https';
 import ip from 'ip';
+import path from 'path';
 import fs from 'fs';
 import fsp from 'fs/promises';
 import session from 'express-session';
 import config from './config.js';
+
+const PUBLIC_VID_PATH = 'vids/';
 
 const app = express();
 app.use(cors());
@@ -21,9 +24,34 @@ app.use(
 );
 
 app.get('/vid', async (req, res) => {
-    const vids = await fsp.readdir('./public/vids');
+    const vids = await fsp.readdir('./public/' + PUBLIC_VID_PATH);
     const vid = vids[Math.floor(Math.random() * vids.length)];
     res.send('./vids/' + vid);
+});
+
+const interrupts = []
+fs.watch('./public/' + PUBLIC_VID_PATH, (eventType, filename) => {
+    if (!filename.endsWith('.mp4') & !filename.endsWith('.webp')) {
+        return;
+    }
+    const publicPath = PUBLIC_VID_PATH + filename;
+    if (eventType === 'change' && !interrupts.includes(publicPath)) {
+        interrupts.push(publicPath);
+    }
+});
+
+app.get('/interrupts', async (req, res) => {
+    res.json(interrupts);
+});
+
+app.delete('/interrupts/:filename', async (req, res) => {
+    const index = interrupts.indexOf(req.params.filename);
+    if (index !== -1) {
+        interrupts.splice(index, 1);
+        res.send('OK');
+    } else {
+        res.status(404).send('Not found');
+    }
 });
 
 https
--- public/main.js
+++ public/main.js
@@ -1,27 +1,57 @@
-const vid1 = document.querySelector('#vid1');
-const vid2 = document.querySelector('#vid2');
+/** @type { HTMLVideoElement } */
+const vidMain = document.querySelector('#vid-main');
+/** @type { HTMLVideoElement } */
+const vidInterrupt = document.querySelector('#vid-interrupt');
 
 async function loopRandomVids() {
-    let vid = vid1;
-    let vidBuffer = vid2;
-    vid.src = await fetch('./vid').then(res => res.text());
     while (true) {
-        const src = await fetch('./vid').then(res => res.text());
-        const onLoaded = new Promise(res => {
-            vidBuffer.addEventListener('loadeddata', res, { once: true });
-        })
-        vidBuffer.src = src;
-        vidBuffer.currentTime = 0;
-        vidBuffer.pause();
-        await new Promise(res => setTimeout(res, 5*1000));
-        await onLoaded;
-        [vid, vidBuffer] = [vidBuffer, vid];
-        vid.classList.remove('hide');
-        vidBuffer.classList.add('hide');
-        vid.play();
+        vidMain.src = await fetch('./vid').then(res => res.text());
+        while (true) {
+            try {
+                await vidMain.play();
+                break;
+            } catch (e) {
+                console.warn('Click anywhere in browser to play video');
+                await new Promise(res => setTimeout(res, 0.5 * 1000));
+            }
+        }
+        await new Promise(res => vidMain.addEventListener('loadeddata', res, {once: true}));
+        const minDurationSecs = Math.max(60, vidMain.duration);
+        await new Promise(res => setTimeout(res, minDurationSecs * 1000));
     }
 }
 loopRandomVids();
+
+async function interruptWithNewVids() {
+    while (true) {
+        const interrupts = await fetch('./interrupts').then(res => res.json());
+        if (interrupts.length === 0) {
+            await new Promise(res => setTimeout(res, 3 * 1000));
+        } else {
+            vidMain.pause();
+            vidInterrupt.classList.remove('hide');
+            for (const interrupt of interrupts) {
+                vidInterrupt.src = interrupt;
+                vidInterrupt.currentTime = 0;
+                while (true) {
+                    try {
+                        await vidInterrupt.play();
+                        break;
+                    } catch (e) {
+                        console.warn('Click anywhere in browser to play video');
+                        await new Promise(res => setTimeout(res, 0.5 * 1000));
+                    }
+                }
+                await fetch('./interrupts/' + encodeURIComponent(interrupt), {method: 'DELETE'});
+                await new Promise(res => setTimeout(res, 60 * 1000));
+            }
+            vidInterrupt.pause();
+            vidInterrupt.classList.add('hide');
+            vidMain.play();
+        }
+    }
+}
+interruptWithNewVids();
 
 async function playNewVid() {
 }
