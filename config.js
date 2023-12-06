907836859ae953b9d1bf4fc27de049c34b2d50b2
Momin Khan <foolmoron@gmail.com>
2023-12-06 01:03:56 -0500

    fix port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
     SECRET: '[replace these config strings when installing the app]',
     PASSWORD: 'test',
-    HTTPS_PORT: 8443,
+    HTTPS_PORT: 8453,
     KEY_PATH: './cert/privkey.key',
     CERT_PATH: './cert/cert.pem',
 };
